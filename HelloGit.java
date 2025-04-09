84b5dc357a95aac8d754e77a3e561238634a4b0b
maochuandong <3402138427@qq.com>
2025-04-09 14:56:42 +0800

    feature-game first version

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,11 @@
 public class HelloGit{
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
+		game_1();
+	}
+	public static int game_1(){
+		Random r = new Random();
+		int randNum = r.nextInt(100)+1;
+		return randNum;
 	}
 }
