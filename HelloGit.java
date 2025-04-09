582ad3b1394d9963dd7c1422e9039bfd55f40ba7
maochuandong <3402138427@qq.com>
2025-04-09 17:06:42 +0800

    Update HelloGit.java

--- HelloGit.java
+++ HelloGit.java
@@ -2,7 +2,6 @@ public class HelloGit{
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
 		int num = game_1();
-		num++;
 		System.out.println(num);
 	}
 	public static int game_1(){
