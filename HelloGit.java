406faacf94ce21df91c977844736f5463175a916
maochuandong <3402138427@qq.com>
2025-04-09 15:12:23 +0800

    hot-fix second version

--- HelloGit.java
+++ HelloGit.java
@@ -2,6 +2,7 @@ public class HelloGit{
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
 		int num = game_1();
+		System.out.println(num);
 	}
 	public static int game_1(){
 		Random r = new Random();
